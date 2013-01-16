e1ef144869cfb5be21048f115b98845d812b25ce
Ben Aldred <benaldred@gmail.com>
2013-01-16 07:55:04 +0000

    archives links helper

--- app/helpers/wordpressed/application_helper.rb
+++ app/helpers/wordpressed/application_helper.rb
@@ -1,4 +1,13 @@
 module Wordpressed
   module ApplicationHelper
+    def archive_links
+      Wordpressed::Post.by_month.map do |p|
+        date = DateTime.parse("#{p}-1")
+        OpenStruct.new(
+          title: date.strftime("%B %Y"),
+          month: date.strftime("%m"),
+          year: date.strftime("%Y"))
+      end
+    end
   end
 end
